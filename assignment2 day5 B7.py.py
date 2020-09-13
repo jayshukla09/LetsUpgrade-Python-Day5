97abc6b0847d9a658a0adb281e412f906459a24c
jayshukla09 <70716742+jayshukla09@users.noreply.github.com>
2020-09-13 18:53:44 +0530

    Add files via upload

---
+++ assignment2 day5 B7.py.py
@@ -0,0 +1,17 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:37:02) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> lower = 1
+upper = 2500
+
+
+def prime(lower, upper):
+    for num in range(lower, upper + 1):
+        if num > 1:
+            for i in range(2, num):
+                if (num % i) == 0:
+                    break
+            else:
+                print(num)
+
+    print("Prime numbers between", lower, "and", upper)
+    filterlist = filter(prime(lower, upper+1),num)
